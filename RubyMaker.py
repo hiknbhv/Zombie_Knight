600c3c890d5017ca3e566107056202ec858c3526
pimpttam000 <pimpttam000@warren.k12.in.us>
2025-04-29 09:50:57 -0400

    Day 2

---
+++ RubyMaker.py
@@ -0,0 +1,63 @@
+import pygame
+
+
+class RubyMaker(pygame.sprite.Sprite):
+    """A tile that is animated.  A ruby will be generated here."""
+
+    def __init__(self, x, y, main_group):
+        """Initialize the ruby maker"""
+        super().__init__()
+
+        #Animation frames
+        self.ruby_sprites = []
+
+        #TODO: create a self.ruby_sprites variables and assign an empty list to it.  HINT:  []
+
+        #Rotating
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile000.png"), (64, 64)))
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile001.png"), (64, 64)))
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile002.png"), (64, 64)))
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile003.png"), (64, 64)))
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile004.png"), (64, 64)))
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile005.png"), (64, 64)))
+        self.ruby_sprites.append(
+            pygame.transform.scale(pygame.image.load("./assets/images/ruby/tile006.png"), (64, 64)))
+        #TODO: so we've just added an image to our list of ruby_sprites.  repeate the previous line form tile's 001 to 006
+
+        #Load image and get rect
+        self.current_sprite = 0
+        self.image = ruby_sprites[self.current_sprite]
+        self.image = self.image.get_rect()
+        self.rect.bottomleft = (x,y)
+
+        #TODO: assign 0 to self.current_sprite.  HINT:  When I say assign y to x I mean x = y
+        #TODO: assign self.ruby_sprites[self.current_sprite] to self.image
+        #TODO: assign self.image.get_rect() to self.image
+        #TODO: assign (x, y) to self.rect.bottomleft
+
+        #Add to the main group for drawing purposes
+        main_group.add(self)
+        #TODO: call main_group's add() method passing self
+
+    def update(self):
+        """Update the ruby maker"""
+        self.animate(self.ruby_sprites, 0.25)
+        #TODO: call self.animate() passing in self.ruby_sprites and 0.25 into the function
+
+    def animate(self, sprite_list, speed):
+        """Animate the ruby maker"""
+        if self.current_sprite < len(sprite_list) - 1:
+            self.current_sprite += speed
+        else:
+            self.current_sprite = 0
+        self.image = sprite_list[int(self.current_sprite)]
+        #TODO: check if self.current_sprite is less than len(sprite_list) - 1.  If so add speed to self.current_sprite
+        #TODO: else assign 0 to self.current_sprite
+
+        #TODO: assign sprite_list[int(self.current_sprite)] to self.image.  When I
---
+++ Tile.py
@@ -0,0 +1,42 @@
+import pygame
+
+class Tile(pygame.sprite.Sprite):
+    """A class to represent a 32x32 pixel area in our display"""
+
+    def __init__(self, x, y, image_int, main_group, sub_group=""):
+        super().__init__()
+        #Load in the correct image and add it to the correct sub group
+        #Dirt tiles
+        if image_int == 1:
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (1).png"), (32, 32))
+        #Platform tiles
+        elif image_int == 2:
+            self.image = self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (2).png"), (32, 32))
+            sub_group.add(self)
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (2).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+        elif image_int == 3:
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (3).png"), (32, 32))
+            sub_group.add(self)
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (3).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+        elif image_int == 4:
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (4).png"), (32, 32))
+            sub_group.add(self)
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (4).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+        elif image_int == 5:
+            self.image = pygame.transform.scale(pygame.image.load("assets/images/tiles/Tile (5).png"), (32, 32))
+            sub_group.add(self)
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (5).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+
+        #Add every tile to the main group
+        main_group.add(self)
+
+        #Get the rect of the image and position within the grid
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+        #Create a mask for better player collisions
+        self.mask = pygame.mask.from_surface(self.image)
